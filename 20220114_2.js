f0db3f824e2b3322d0e9ad72d2a9ee02e63940f1
刘宣龙 <liuxuanlong@keepyoga.com>
2022-01-14 14:50:57 +0800

    3. 无重复字符的最长子串

---
+++ 20220114_2.js
@@ -0,0 +1,25 @@
+// 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+
+;(function () {
+	const str = "afeabcdefabcbb"
+	//用于记录字符是否出现过
+	const strSet = new Set()
+	const len = str.length
+	// rightPointer为右指针，result为最后的结果
+	let rightPointer = -1,
+		result = 0
+	//i为左指针
+	for (let i = 0; i < len; i++) {
+		//左指针向右移动，删除一个字符
+		if (i != 0) {
+			strSet.delete(str.charAt(i - 1))
+		}
+		// 右指针向右移动,set增加一个字符
+		while (rightPointer + 1 < len && !strSet.has(str.charAt(rightPointer + 1))) {
+			strSet.add(str.charAt(rightPointer + 1))
+			rightPointer++
+		}
+		result = Math.max(result, rightPointer - i + 1)
+	}
+	console.log(result)
+})()
