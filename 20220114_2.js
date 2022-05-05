b648e7eeb31cdce1a621d986c1be328f0722db56
liuxuanlong <1060508332@qq.com>
2022-05-06 00:08:43 +0800

    算法练习

--- 20220114_2.js
+++ 20220114_2.js
@@ -1,25 +1,26 @@
-// 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
-//时间复杂度：O(N),左右指针分别会遍历一遍字符串
-;(function () {
-	const str = "afeabcdefabcbb"
-	//用于记录字符是否出现过
-	const strSet = new Set()
-	const len = str.length
-	// rightPointer为右指针，result为最后的结果
-	let rightPointer = -1,
-		result = 0
-	//i为左指针
-	for (let i = 0; i < len; i++) {
-		//左指针向右移动，删除一个字符
-		if (i != 0) {
-			strSet.delete(str.charAt(i - 1))
-		}
-		// 右指针向右移动,set增加一个字符
-		while (rightPointer + 1 < len && !strSet.has(str.charAt(rightPointer + 1))) {
-			strSet.add(str.charAt(rightPointer + 1))
-			rightPointer++
-		}
-		result = Math.max(result, rightPointer - i + 1)
-	}
-	console.log(result)
-})()
+/**
+ * 时间复杂度:O(n)
+ * 空间复杂度：O(m)，其中 m 表示字符集的大小。
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  const len = s.length;
+  let rightPoint = 0,
+    result = 0;
+  const set = new Set();
+  for (let i = 0; i < len; i++) {
+    const chars = s.charAt(i - 1);
+    //左指针向右移动，set删除一个字符
+    if (i != 0) {
+      set.delete(chars);
+    }
+    //右指针向右移动，set增加一个字符
+    while (!set.has(s.charAt(rightPoint)) && rightPoint < len) {
+      set.add(s.charAt(rightPoint));
+      rightPoint++;
+    }
+    result = Math.max(result, rightPoint - i);
+  }
+  return result;
+};
