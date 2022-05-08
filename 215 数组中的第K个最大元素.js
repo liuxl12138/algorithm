1a93e97d4ec6ea2cd5c54d1f3a1ff8f4b49a4088
liuxuanlong <1060508332@qq.com>
2022-05-08 23:36:41 +0800

    修改文件名

--- 215 数组中的第K个最大元素.js
+++ 215 数组中的第K个最大元素.js
@@ -1,42 +1,56 @@
-let quikSelect = function (arr, left, right, k) {
-    if (left < right) {
-        let midIndex = findMid(arr, left, right)
-        if (midIndex === k) {
-            return arr[midIndex]
-        } else if (k > midIndex) {
-            return quikSelect(arr, midIndex + 1, right, k)
-        } else {
-            return quikSelect(arr, left, midIndex - 1, k)
-        }
-    }
-    return arr[left]
+//参考：https://github.com/sisterAn/JavaScript-Algorithms/issues/62
 
-    function findMid(arr, left, right) {
-        let i = left, j = right
-        let baseIndex = Math.floor(Math.random() * (right - left + 1)) + left
-        swap(arr,left, baseIndex)
-        let base = arr[left]
-        while (i !== j) {
-            while (arr[j] >= base && i < j) {
-                j--
+        //使用快速排序思想来解决
+        //快速排序过程中，递归的每一次都会确定中间值的位置,所以在这个过程中，判断中间值的位置是否就是要查找的值的位置
+        //判断要查找值所在的区间，另一个区间不用查找，就可以提交效率
+        //因为要查找的值为第k大的元素，所以他在排序好的数组中的位置就是arr.length-k
+        //可以使用随机化来优化快速排序
+        //时间复杂度O(n)
+        //空间复杂度O(logn)
+        let quickSelect = function (arr, left, right, kIndex) {
+            if (left < right) {
+                let index = findMid(arr, left, right)
+                if (index === kIndex) {
+                    return arr[index]
+                } else if (kIndex < index) {
+                    return quickSelect(arr, left, index - 1, kIndex)
+                } else {
+                    return quickSelect(arr, index + 1, right, kIndex)
+                }
             }
-            while (arr[i] <= base && i < j) {
-                i++
-            }
-            if (i < j) {
-                swap(arr,i, j)
-            }
+            return arr[left]
         }
-        swap(arr,i, left)
-        return i
-    }
-    function swap(arr,index1, index2) {
-        let tmp = arr[index1]
-        arr[index1] = arr[index2]
-        arr[index2] = tmp
-    }
-}
-let findKthLargest = function (nums, k) {
-    return quikSelect(nums, 0, nums.length - 1, nums.length - k)
+        let swap = function (arr,index1,index2){
+            let tmp = arr[index1]
+            arr[index1] = arr[index2]
+            arr[index2] = tmp
+        }
+        var findMid = function (arr, left, right) {
+            //随机选取
+            let randomIndex= Math.floor(Math.random() *(right -left +1)) +left
+            swap(arr,left,randomIndex)
+            let base = arr[left]
+            let i = left, j = right;
+            let tmp
+            while (i != j) {
+                while (arr[j] >= base && i < j) {
+                    j--
+                }
+                while (arr[i] <= base && i < j) {
+                    i++
+                }
+                if (i < j) {
+                    tmp = arr[i]
+                    arr[i] = arr[j]
+                    arr[j] = tmp
+                }
+            }
+            arr[left] = arr[i]
+            arr[i] = base
+            return i
+        }
+        let findKthLargest = function (nums, k) {
+            return quickSelect(nums, 0, nums.length - 1, nums.length - k)
+        };
 
-};
+        console.log(findKthLargest([1, 1, 1, 1], 2),'result') 
